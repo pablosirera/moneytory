cc29408a82170ba989588bddb63522c984dee521
Pablo Sirera <psirera4@gmail.com>
2022-06-06 10:59:19 +0200

    create app

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import BaseStat from '@/components/BaseStat.vue'
+import SwapTheme from './components/SwapTheme.vue'
+import CompactTable from './components/CompactTable.vue'
+import TableActionsButtons from '@/components/TableActionsButtons.vue'
+</script>
+
+<template>
+  <header class="p-6 border-b-2 flex justify-between items-center">
+    <h1 class="font-bold uppercase">MoneyTory</h1>
+    <SwapTheme />
+  </header>
+  <main class="m-6">
+    <section class="flex justify-between items-center">
+      <BaseStat title="Dinero total" value="1000" />
+      <div>
+        <TableActionsButtons />
+      </div>
+    </section>
+    <section class="mt-10">
+      <CompactTable />
+    </section>
+  </main>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/BaseStat.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  value: {
+    type: [Number, String],
+    default: 0,
+  },
+  desc: {
+    type: String,
+    default: '',
+  },
+})
+
+const formattedValue = computed(() => {
+  const euroLocale = Intl.NumberFormat('es-ES', {
+    style: 'currency',
+    currency: 'EUR',
+  })
+  return euroLocale.format(props.value)
+})
+</script>
+
+<template>
+  <div class="stats shadow">
+    <div class="stat">
+      <div class="stat-title">{{ title }}</div>
+      <div class="stat-value text-primary">{{ formattedValue }}</div>
+      <div class="stat-desc">{{ desc }}</div>
+    </div>
+  </div>
+</template>
